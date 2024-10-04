6eed6fe9c47b66186b77f03bd9122de1bff58c35
KevinORIS <c.s.2015@mail.ru>
2024-10-04 13:58:58 +0300

    first commit

---
+++ src/main/java/by/oris/game/webmafia/WebMafiaApplication.java
@@ -0,0 +1,13 @@
+package by.oris.game.webmafia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMafiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMafiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/oris/game/webmafia/WebMafiaApplicationTests.java
@@ -0,0 +1,13 @@
+package by.oris.game.webmafia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMafiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
