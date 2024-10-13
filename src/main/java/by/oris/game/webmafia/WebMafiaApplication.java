469aae86af426fe621cd9be32695282b95b6ebda
KevinORIS <c.s.2015@mail.ru>
2024-10-13 15:57:41 +0300

    update

--- src/main/java/by/oris/game/webmafia/WebMafiaApplication.java
+++ src/main/java/by/oris/game/webmafia/WebMafiaApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebMafiaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebMafiaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WebMafiaApplication.class, args);
+    }
 }
