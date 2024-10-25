131e2529ab68b913d725db5cbe67ca88ac9c1e8e
KevinORIS <c.s.2015@mail.ru>
2024-10-25 16:30:32 +0300

    Update Repositories

--- src/main/java/by/oris/game/webmafia/repository/outgame/FriendshipRepository.java
+++ src/main/java/by/oris/game/webmafia/repository/outgame/FriendshipRepository.java
@@ -2,6 +2,8 @@ package by.oris.game.webmafia.repository.outgame;
 
 import by.oris.game.webmafia.model.entities.outgame.Friendship;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FriendshipRepository extends JpaRepository<Friendship, Integer> {
 }
